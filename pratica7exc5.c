e00af54f0927ecf297ce1cd890ff31ff9cd4f24c
joaoclemos <joaoclemos22@gmail.com>
2025-10-17 14:48:19 -0300

    Q5

--- pratica7exc5.c
+++ pratica7exc5.c
@@ -2,22 +2,15 @@
 
 int main(void) {
     int n;
-
-    // Lê a quantidade de elementos
     scanf("%d", &n);
-
-    // Declara o vetor com o tamanho lido
     int v[n];
 
-    // Lê cada elemento do vetor
     for (int i = 0; i < n; i++) {
         scanf("%d", &v[i]);
     }
 
-    // Algoritmo de ordenação (Bubble Sort)
     for (int i = 0; i < n - 1; i++) {
         for (int j = 0; j < n - i - 1; j++) {
-            // Se o elemento atual for menor que o próximo, troca eles de lugar
             if (v[j] < v[j + 1]) {
                 int temp = v[j];
                 v[j] = v[j + 1];
@@ -26,9 +19,11 @@ int main(void) {
         }
     }
 
-    // Imprime o vetor ordenado
     for (int i = 0; i < n; i++) {
-        printf("%d ", v[i]);
+        printf("%d", v[i]);
+        if (i < n - 1) {
+            printf(" ");
+        }
     }
     printf("\n");
 
